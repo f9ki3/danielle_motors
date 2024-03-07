27267a47fa612521fe7c9cd347c375f85b03c9c5
DySyre <119650160+DySyre@users.noreply.github.com>
2024-03-07 23:58:52 +0800

    Revise the first version UI here
    What I done tonight is
    
    - Import danielle_motors.sql to dms_db.sql inadd bale admin saka rack at shelf
    - tinanggal ko sa folder yung version 1 nilabas ko ulit then ung module nalang ng inventory i add after macomplete

--- admin/header.php
+++ admin/header.php
@@ -4,6 +4,9 @@
     <title>Admin</title>
     <link rel="icon" href="../static/img/logo.png" type="image/x-icon">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"> 
+    try to import water css bootstrap
+-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../static/css/navigation_bar.css">
     <link rel="stylesheet" href="../static/css/product.css">
--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "danielle_motors";
+$dbname = "dms_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
