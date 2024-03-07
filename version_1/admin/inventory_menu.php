2a4aa72db4e47e57ed3a076f0cb1b11b1a1a8496
Fyke Lleva <floterina@gmail.com>
2024-03-07 18:40:47 +0800

    migrate by f9ki3

---
+++ version_1/admin/inventory_menu.php
@@ -0,0 +1,8 @@
+<div style="background-color: white;" class="rounded border p-3 mb-3 w-100">
+            <h5 class="fw-bolder">Inventory</h5>
+            <a href="inventory" class="btn btn-sm border rounded mb-2">Products</a>
+            <a href="inventory_stock-in" class="btn btn-sm border rounded mb-2">Stock-in</a>
+            <a href="inventory_stock-out" class="btn btn-sm border rounded mb-2">Stock-out</a>
+            <a href="inventory_sell_item" class="btn btn-sm border rounded mb-2">Sell Items</a>
+            <a href="inventory_rack_identification" class="btn btn-sm border rounded mb-2">Rack Identification</a>
+</div>
---
+++ version_1/admin/inventory_stock-out.php
@@ -0,0 +1,70 @@
+
+<?php include 'session.php'?>
+<html lang="en">
+<?php include 'header.php'?>
+<body>
+<div style="display: flex; flex-direction: row">
+<?php include 'navigation_bar.php'?>
+
+<!-- start stock-in-->
+
+<div style="width: 100%" class="content p-3">
+    <div>
+        <?php include 'inventory_menu.php'?>
+
+        <div style="background-color: white; height: 90vh;" class="rounded border p-3 mb-3 w-100">
+            <div class="row">
+                <h6>Stocks Out</h6>
+                <div class="col-12 col-md-3 p-2">
+                    <div class="border p-2 rounded text-center">+ Add</div>
+                </div>
+
+                <?php 
+                    for ($i = 0; $i <= 20; $i++) {
+                        $letter = chr(65 + $i); // 65 is tde ASCII value for 'A'
+                        echo '<div class="col-6 col-md-3 p-2 text-center">
+                        <div class="border p-2 rounded">RACK - ' . $letter . '</div>
+                        </div>';
+                    }
+                ?>
+
+
+
+                
+            </div>
+            
+        </div>
+
+        <!-- Add Modal -->
+        <div class="modal fade mt-5" id="add_product" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Product</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="text" class="form-control form-control-sm mb-2" placeholder="SR NO.">
+                <input type="text" class="form-control form-control-sm mb-2" placeholder="Product Name">
+                <textarea name="" class="form-control form-control-sm mb-2" placeholder="Description" id="" cols="30" rows="5"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">+ Add</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<!-- end stock-in-->
+
+
+</div>
+<?php include 'footer.php'?>
+</body>
+</html>
