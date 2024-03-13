5f3c0bb1019e240c565e10e10a112d4eaa651487
Christian Azul <53144221+ChristianAzulL@users.noreply.github.com>
2024-03-13 16:48:35 +0800

    product_list

--- admin/store_product_list.php
+++ admin/store_product_list.php
@@ -4,8 +4,12 @@
 <?php include 'header.php'?>
 <body>
 <div style="display: flex; flex-direction: row">
-<?php include 'navigation_bar.php'?>
+<?php
+include 'navigation_bar.php';
+include '../config/config.php';
 
+?>
+<link href="https://cdn.datatables.net/v/dt/dt-2.0.2/datatables.min.css" rel="stylesheet">
 <!-- start inventory-->
 
 <div style="width: 100%" class="content p-3">
@@ -49,31 +53,8 @@
                                 <td scope="col">Action</td>
                             </tr>
                         </thead>
-                        <tbody>
-                        <tr>
-                                <td scope="row" >PROD001</td>
-                                <td >IMG001</td>
-                                <td>Carburator Honda</td>
-                                <td>100 pcs</td>
-                                <td>PHP 100</td>
-                                <td>PHP 100</td>
-                                <td>PHP 100</td>
-                                <td>PHP 100</td>
-                                <td>PHP 100</td>
-                                <td>PHP 100</td>
-                                <td>PHP 100</td>
-                                <td class="text-end">
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                                    </svg> </button>
-
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-                                    </svg> </button>
-                                </td>
-                        </tr>
-                        
-                            
+                        <tbody id="MaterialTableBody">
+                            <!-- dynamic populate -->
                         </tbody>
                     </table>
                 </div>
@@ -115,3 +96,30 @@
 <?php include 'footer.php'?>
 </body>
 </html>
+
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.datatables.net/v/dt/dt-2.0.2/datatables.min.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function () {
+    // Initialize DataTable
+    var table = $('#tabledataMaterial').DataTable({
+        "fnCreatedRow": function (nRow, aData, iDataIndex) {
+            $(nRow).attr('id', aData[0]);
+        },
+        'serverSide': 'true',
+        'processing': 'true',
+        'paging': 'true',
+        'order': [],
+        'ajax': {
+            'url': 'store_stocks_fetch.php',
+            'type': 'post',
+        },
+        "aoColumnDefs": [{
+            "bSortable": false,
+            "aTargets": [6] // Adjust the index to match the actual number of columns
+        }]
+    });
+});
+</script>
--- admin/store_stocks_update.php
+++ admin/store_stocks_update.php
@@ -1,31 +1,38 @@
 <?php
 include '../config/config.php';
 
-// Retrieve data from the AJAX request
-$id = isset($_POST['id']) ? $_POST['id'] : null; // ID of the row to be updated
-$materialDate = $_POST['materialDate'];
-$materialInvoiceNo = $_POST['materialInvoiceNo'];
-$cashierName = $_POST['cashierName'];
-$receivedBy = $_POST['receivedBy'];
-$inspectedBy = $_POST['inspectedBy'];
-$verifiedBy = $_POST['verifiedBy'];
+// Check if 'newId' key is set in the $_POST array
+if(isset($_POST['newId'])) {
+    // Retrieve data from the AJAX request
+    $newId = $_POST['newId'];
+    $materialDate = $_POST['materialDate'];
+    $materialInvoiceNo = $_POST['materialInvoiceNo'];
+    $cashierName = $_POST['cashierName'];
+    $receivedBy = $_POST['receivedBy'];
+    $inspectedBy = $_POST['inspectedBy'];
+    $verifiedBy = $_POST['verifiedBy'];
 
-// Validate and sanitize data if needed...
+    // Update data in the database using the new ID
+    $sql = "UPDATE material_transfer 
+            SET material_date = '$materialDate', 
+                material_invoice = '$materialInvoiceNo', 
+                material_cashier = '$cashierName', 
+                material_recieved_by = '$receivedBy', 
+                material_inspected_by = '$inspectedBy', 
+                material_verified_by = '$verifiedBy' 
+            WHERE id = $newId";
 
-// Update data in the database
-$sql = "UPDATE material_transfer 
-        SET material_date = '$materialDate', 
-            material_invoice = '$materialInvoiceNo', 
-            material_cashier = '$cashierName', 
-            material_recieved_by = '$receivedBy', 
-            material_inspected_by = '$inspectedBy', 
-            material_verified_by = '$verifiedBy' 
-        WHERE id = $id";
+    echo "SQL Query: " . $sql;
 
-if (mysqli_query($conn, $sql)) {
-    echo "Data updated successfully!";
+    // Update data in the database
+    if (mysqli_query($conn, $sql)) {
+        echo "Data updated successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    // Handle the case when 'newId' key is not set
+    echo "Error: 'newId' key is not set in the POST request.";
 }
 
 mysqli_close($conn);
