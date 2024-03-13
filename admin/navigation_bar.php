bc39c500eaa81795dd5b30276e2f724d09784d68
Fyke Lleva <floterina@gmail.com>
2024-03-13 08:34:17 +0800

    navbar error

--- admin/navigation_bar.php
+++ admin/navigation_bar.php
@@ -6,7 +6,7 @@
     <h5 class="fw-bolder ms-2 mt-4">Danielle Motors</h5>
     <hr>
     <div>
-        <li><a href="/danielle_motors/admin/dashboard" class=" menu-btn btn w-100 text-start  rounded mb-3"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi me-3 bi-box2-fill" viewBox="0 0 16 16">
+        <li><a href="/dannielle_motors/admin/dashboard" class=" menu-btn btn w-100 text-start  rounded mb-3"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi me-3 bi-box2-fill" viewBox="0 0 16 16">
         <path d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4zM15 4.667V5H1v-.333L1.5 4h6V1h1v3h6z"/>
         </svg> Dashboard</a></li>
         
