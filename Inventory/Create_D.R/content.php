49f7d9cf054bed701dc1b87f8eb2f200021a3c91
Christian Azul <53144221+ChristianAzulL@users.noreply.github.com>
2024-03-16 00:19:38 +0800

    pacheck boss
    naglakay ako session_start sa mga index ng inventory then delivery receipt

--- Inventory/Create_D.R/content.php
+++ Inventory/Create_D.R/content.php
@@ -1,35 +1,39 @@
-<div class="row">
-    <h1 class="mb-5">Delivery Receipt Details</h1>
-    <hr class="mb-5">
-    <div class="col-lg-8">
-        <div class="form-floating">
-            <select class="form-select" id="supplier_id" name="supplier_id" aria-label="Floating label select example">
-                <option selected="">Select Supplier</option>
+<form action="../../PHP - process_files/create_dr.php" method="POST">
+    <div class="row">
+    
+        <h1 class="mb-5">Delivery Receipt Details</h1>
+        <hr class="mb-5">
+        <div class="col-lg-8">
+            <select class="form-select" id="supplier_id" name="supplier_id" aria-label="Floating label select examplerequired"  data-choices="data-choices" data-options='{"removeItemButton":true,"placeholder":true}'>
+                <option selected="" value="">Select Supplier</option>
                 <?php include "supplier_option.php"; ?>
             </select>
-            <label for="floatingSelect">Works with selects</label>
         </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="form-floating mb-3">
-            <input class="form-control" id="floatingInput" type="email" placeholder="name@example.com" />
-            <label for="floatingInput">Checked by</label>
+        <div class="col-lg-4">
+            <div class="form-floating mb-3">
+                <input class="form-control" id="checked_by" name="checked_by" type="text" placeholder="Enter full name" required/>
+                <label for="floatingInput">Checked by</label>
+            </div>
         </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="form-floating mb-3">
-            <input class="form-control" id="floatingInput" type="email" placeholder="name@example.com" />
-            <label for="floatingInput">Approved by</label>
+        <div class="col-lg-4">
+            <div class="form-floating mb-3">
+                <input class="form-control" id="approved_by" name="approved_by" type="text" placeholder="Enter full name" required/>
+                <label for="floatingInput">Approved by</label>
+            </div>
         </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="form-floating mb-3">
-            <input class="form-control" id="floatingInput" type="email" placeholder="name@example.com" />
-            <label for="floatingInput">Delivered by</label>
+        <div class="col-lg-4">
+            <div class="form-floating mb-3">
+                <input class="form-control" id="delivered_by" name="delivered_by" type="text" placeholder="Enter full name" required/>
+                <label for="floatingInput">Delivered by</label>
+            </div>
         </div>
+        <div class="col-lg-4">
+            <label class="form-label" for="datepicker">Recieved Date</label>
+            <input class="form-control datetimepicker" name="received_date" id="received_date" type="text" placeholder="dd/mm/yyyy" data-options='{"disableMobile":true,"dateFormat":"d/m/Y"}' required/>
+        </div>
+        <div class="col-lg-12 text-end">
+            <button class="btn btn-primary" type="submit">Next</button>
+        </div>
+
     </div>
-    <div class="col-lg-4">
-    <label class="form-label" for="datepicker">Start Date</label>
-        <input class="form-control datetimepicker" id="datepicker" type="text" placeholder="dd/mm/yyyy" data-options='{"disableMobile":true,"dateFormat":"d/m/Y"}' />
-    </div>
-</div>
+</form>
