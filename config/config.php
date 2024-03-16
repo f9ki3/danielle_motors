9b2d48bdaaea11c100765890a1491977ccadf23e
Fyke Lleva <floterina@gmail.com>
2024-03-16 08:20:36 +0800

    database fix -fyke

--- config/config.php
+++ config/config.php
@@ -1,27 +1,22 @@
-<!-- <?php
-// online
+<?php
+// Database connection parameters
 $servername = "sql.freedb.tech";
 $username = "freedb_dmp_master";
 $password = "8@YASU8ypbA2uA%";
 $dbname = "freedb_dmp_db";
 
-// local
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "dms_db";
-
-
 // Create a new MySQLi instance
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check the connection
 if ($conn->connect_error) {
-    // Log the connection error to a file
-    $error_message = "Connection failed: " . $conn->connect_error;
-    file_put_contents('connection_errors.log', $error_message, FILE_APPEND);
-    
-    // Display a generic error message to the user
-    die("Connection failed. Please try again later.");
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
 }
+
+// Further code logic goes here
+
+// Close the connection when done
+$conn->close();
 ?>
--- php/login.php
+++ php/login.php
@@ -2,27 +2,34 @@
 // Start a session
 session_start();
 
-// Establish database connection
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "dms_db";
+// Database connection parameters
+$servername = "sql.freedb.tech";
+$username = "freedb_dmp_master";
+$password = "8@YASU8ypbA2uA%";
+$dbname = "freedb_dmp_db";
 
-// // Online Database Include File
-include_once '../config/config.php';
-$status = 0;
+// Create a new MySQLi instance
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+   
+}
 
 // Check if the POST data is set
-if(isset($_POST['uname']) && isset($_POST['pass'])) {
+if(isset($_POST['uname'], $_POST['pass'])) {
     // Get the posted username and password
     $username = $_POST['uname'];
     $password = $_POST['pass'];
+
     // Prepare a SQL statement to check if the username and password exist
     $sql = "SELECT * FROM admin WHERE username = ? AND password = ? AND status = ?";
     $stmt = $conn->prepare($sql);
+
+    // Bind parameters
+    $status = 0; // Assuming status is an integer
     $stmt->bind_param("ssi", $username, $password, $status);
 
     // Execute the statement
@@ -35,7 +42,7 @@ if(isset($_POST['uname']) && isset($_POST['pass'])) {
     if ($result->num_rows > 0) {
         // Fetch the user data
         $row = $result->fetch_assoc();
-        
+
         // Set session variables
         $_SESSION['id'] = $row['id'];
         $_SESSION['fname'] = $row['fname'];
@@ -45,14 +52,13 @@ if(isset($_POST['uname']) && isset($_POST['pass'])) {
         $_SESSION['img'] = $row['img'];
         $_SESSION['username'] = $username;
         $_SESSION['loggedin'] = true;
-        
+
         // Respond with '1' to indicate successful login
         echo '1';
     } else {
         // Respond with '0' to indicate failed login
         echo '0';
     }
-
 } else {
     // Respond with '0' to indicate failed login
     echo '0';
