a4028284b927253f8267fabe7837aabd2f093a2e
Fyke Lleva <floterina@gmail.com>
2024-03-23 18:01:07 +0800

    update sales fyke

--- admin/sales.php
+++ admin/sales.php
@@ -5,6 +5,13 @@
 <body>
 <div style="display: flex; flex-direction: row">
 <?php include 'navigation_bar.php'?>
+<?php 
+include '../config/config.php';
+
+// SQL query to fetch data from the table and sort by TransactionDate in descending order
+$sql = "SELECT * FROM purchase_transactions ORDER BY TransactionDate DESC";
+$result = $conn->query($sql);
+?>
 
 <!-- start sales-->
 
@@ -12,15 +19,14 @@
     <div>
         <div style="background-color: white;" class="rounded border p-3 mb-3 w-100">
             <h5 class="fw-bolder">Sales</h5>
-            <button class="btn btn-sm border rounded mb-2">All Sales</button>
-            <button class="btn btn-sm border rounded mb-2">Walk-in Sales</button>
+            <button class="btn btn-sm border rounded mb-2 btn-primary">Walk-in Sales</button>
             <button class="btn btn-sm border rounded mb-2">Delivery Sales</button>
             <button class="btn btn-sm border rounded mb-2">Online Sales</button>
             <button class="btn btn-sm border rounded mb-2">Balance Account</button>
             
         </div>
 
-        <div style="background-color: white; height: 90vh;" class="rounded border p-3 mb-3 w-100">
+        <div style="background-color: white; height: 80vh;" class="rounded border p-3 mb-3 w-100">
             <div class="row">
                 <div style="display: flex; justify-content: space-between; align-items: center;"> 
                     <div style="width: 50%">
@@ -28,102 +34,53 @@
                     </div>
 
                     <div>
-                        <button class="btn border btn-sm rounded">Purchase</button>
-                        <button class="btn border btn-sm rounded">Cart</button>
+                        <!-- <button class="btn border btn-sm rounded">Purchase</button>
+                        <button class="btn border btn-sm rounded">Cart</button> -->
                     </div>
                 </div>
-                <div class="p-1">
+                <div class="p-1" style="height: 700px; overflow: auto;">
                     <table class="table mt-3">
                         <thead>
                             <tr>
-                            <th scope="col" width="10%" class="product_id">Product Id</th>
-                            <th scope="col" width="20%" class="product_img">Image</th>
-                            <th scope="col" width="40%">Product Name</th>
-                            <th scope="col" width="10%">Stocks</th>
-                            <th scope="col" width="20%" class="text-end">Action</th>
+                                <th scope="col" width="10%" >Transaction Code</th>
+                                <th scope="col" width="10%" >Transaction Date</th>
+                                <th scope="col" width="10%">Customer Name</th>
+                                <th scope="col" width="10%">Payment Method </th>
+                                <th scope="col" width="5%">Subtotal</th>
+                                <th scope="col" width="5%">Tax</th>
+                                <th scope="col" width="5%">Discount</th>
+                                <th scope="col" width="5%">Total</th>
+                                <th scope="col" width="5%">Payment</th>
+                                <th scope="col" width="5%">Change</th>
                             </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                                <td scope="row" class="product_id">PROD001</td>
-                                <td class="product_img">IMG001</td>
-                                <td>Carburator Honda</td>
-                                <td>100 pcs</td>
-                                <td class="text-end">
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                                    </svg> </button>
+                        <?php
+                            // Fetch and display data from the table
+                            if ($result->num_rows > 0) {
+                                while($row = $result->fetch_assoc()) {
+                                    echo "<tr>";
+                                    echo "<td>".$row["TransactionID"]."</td>";
+                                    echo "<td>".$row["TransactionDate"]."</td>";
+                                    echo "<td>".$row["CustomerName"]."</td>";
+                                    echo "<td>".$row["TransactionType"]."</td>";
 
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-                                    </svg> </button>
-                                </td>
-                            
-                            </tr>
-                            <tr>
-                                <td scope="row" class="product_id">PROD001</td>
-                                <td class="product_img">IMG001</td>
-                                <td>Carburator Honda</td>
-                                <td>100 pcs</td>
-                                <td class="text-end">
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                                    </svg> </button>
+                                    // Format currency fields to pesos
+                                    echo "<td> ₱ ".number_format($row["Subtotal"], 2, '.', ',')."</td>";
+                                    echo "<td> ₱ ".number_format($row["Tax"], 2, '.', ',')."</td>";
+                                    echo "<td> ₱ ".number_format($row["Discount"], 2, '.', ',')."</td>";
+                                    echo "<td> ₱ ".number_format($row["Total"], 2, '.', ',')."</td>";
+                                    echo "<td> ₱ ".number_format($row["Payment"], 2, '.', ',')."</td>";
+                                    echo "<td> ₱ ".number_format($row["ChangeAmount"], 2, '.', ',')."</td>";
 
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-                                    </svg> </button>
-                                </td>
-                            
-                            </tr>
-                            <tr>
-                                <td scope="row" class="product_id">PROD001</td>
-                                <td class="product_img">IMG001</td>
-                                <td>Carburator Honda</td>
-                                <td>100 pcs</td>
-                                <td class="text-end">
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                                    </svg> </button>
+                                    echo "</tr>";
+                                }
+                            } else {
+                                echo "<tr><td colspan='15'>No transactions found.</td></tr>";
+                            }
+                            ?>
 
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-                                    </svg> </button>
-                                </td>
                             
-                            </tr>
-                            <tr>
-                                <td scope="row" class="product_id">PROD001</td>
-                                <td class="product_img">IMG001</td>
-                                <td>Carburator Honda</td>
-                                <td>100 pcs</td>
-                                <td class="text-end">
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                                    </svg> </button>
-
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-                                    </svg> </button>
-                                </td>
-                            
-                            </tr>
-                            <tr>
-                                <td scope="row" class="product_id">PROD001</td>
-                                <td class="product_img">IMG001</td>
-                                <td>Carburator Honda</td>
-                                <td>100 pcs</td>
-                                <td class="text-end">
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                                    </svg> </button>
-
-                                    <button class="btn btn-sm border"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-                                    </svg> </button>
-                                </td>
-                            
-                            </tr>
                         </tbody>
                     </table>
                 </div>
