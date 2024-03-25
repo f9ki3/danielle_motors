47727dde1eec1d55daeaea77e0a112a082212793
Alexander Inciong <alexanderinciong1@gmail.com>
2024-03-25 19:06:43 +0800

    FYKE

--- admin/purchase_cart.php
+++ admin/purchase_cart.php
@@ -61,11 +61,7 @@
                     <div class="button-container">
                         <a href="purchase" class="btn border  btn-sm me-5 rounded">Purchase</a>
                         <div class="badge-container">
-                            <a href="purchase_cart" class="btn btn-primary border btn-sm rounded">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                            </svg>
-                            </a>
+                            <a href="purchase_cart" class="btn btn-primary border btn-sm rounded">Cart</a>
                             <span class="badge text-bg-danger" id="counter"></span>
                         </div>
                     </div>
