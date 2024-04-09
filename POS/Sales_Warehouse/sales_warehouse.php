b977e8f6f84b2e9acf09c25b926aab8ba20b6037
Fyke Lleva <floterina@gmail.com>
2024-04-09 13:22:59 +0800

    updated

---
+++ POS/Sales_Warehouse/sales_warehouse.php
@@ -0,0 +1,43 @@
+<?php
+$query = "SELECT * FROM purchase_transactions WHERE TransactionType = 'Walk-in' ORDER BY TransactionDate DESC";
+
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$result = $stmt->get_result(); // Get result set
+
+// Fetch data from the result set
+while ($row = $result->fetch_assoc()) {
+    $TransactionID = $row['TransactionID'];
+    $CustomerName = $row['CustomerName'];
+    $TransactionDate = $row['TransactionDate'];
+    $TransactionAddress = $row['TransactionAddress'];
+    $TransactionVerifiedBy = $row['TransactionVerifiedBy'];
+    $TransactionInspectedBy = $row['TransactionInspectedBy'];
+    $TransactionReceivedBy = $row['TransactionReceivedBy'];
+    $TransactionPaymentMethod = $row['TransactionPaymentMethod'];
+    $TransactionType = $row['TransactionType'];
+    $Subtotal = $row['Subtotal'];
+    $Tax = $row['Tax'];
+    $Discount = $row['Discount'];
+    $Total = $row['Total'];
+    $Payment = $row['Payment'];
+    $ChangeAmount = $row['ChangeAmount'];
+
+    // Output the data
+    echo '
+    <a href="Purchase_Warehouse_Walkin_Receipt?transaction_code=' . $TransactionID . '">
+        <tr>
+            <td class="sort transaction-code white-space-nowrap align-middle" style="width: 5%;"><a href="transaction?id=' . $TransactionID . '">' . $TransactionID . '</a></td>
+            <td class="sort transaction-date white-space-nowrap align-middle" style="width: 15%;"><a href="../Purchase_Warehouse_Walkin_Receipt/?transaction_code=' . $TransactionID . '">' . $TransactionDate . '</a></td>
+            <td class="sort customer-name align-middle text-start" style="width: 10%;">' . $CustomerName . '</td>
+            <td class="sort payment-method align-middle text-start" style="width: 10%;">' . $TransactionPaymentMethod . '</td>
+            <td class="sort subtotal align-middle text-start" style="width: 10%;">' . $Subtotal . '</td>
+            <td class="sort tax align-middle text-start" style="width: 5%;">' . $Tax . '</td>
+            <td class="sort discount align-middle text-start" style="width: 15%;">' . $Discount . '</td>
+            <td class="sort total align-middle text-start" style="width: 10%;">' . $Total . '</td>
+            <td class="sort payment align-middle text-start" style="width: 10%;">' . $Payment . '</td>
+            <td class="sort change align-middle text-start" style="width: 10%;">' . $ChangeAmount . '</td>
+        </tr>
+    </a>';
+}
+?>
