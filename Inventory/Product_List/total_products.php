186d788fcef58d3ea4646dd954099991576c95ce
Alexander Inciong <xnamecf04@gmail.com>
2024-04-10 15:31:26 +0800

    Update total_products.php

--- Inventory/Product_List/total_products.php
+++ Inventory/Product_List/total_products.php
@@ -1,7 +1,13 @@
 <?php
 include_once "../../database/database.php";
 
-$sql = "SELECT COUNT(*) FROM product";
+$sql = "SELECT 
+product.COUNT(*)
+FROM product
+INNER JOIN category ON category.id = product.category_id
+INNER JOIN brand ON brand.id = product.brand_id
+INNER JOIN unit ON unit.id = product.unit_id
+ORDER BY product.id DESC";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
