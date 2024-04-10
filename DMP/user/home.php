1d4b3a0c3f2600ff76ee9910b2b82631767e1618
Jaygun211 <loislancerivera2@gmail.com>
2024-04-10 14:41:30 +0800

    Update home.php

--- DMP/user/home.php
+++ DMP/user/home.php
@@ -49,7 +49,7 @@
     <div class="container text-center">
         <!-- Banner -->
         <div class="banner">
-            <img src="../image/banner.png" alt="Banner Image">
+            <img src="../img/banner.png" alt="Banner Image">
             <div class="banner-content">
                 <h1 class="banner-text">Welcome to Danielle Motor Parts</h1>
             </div>
