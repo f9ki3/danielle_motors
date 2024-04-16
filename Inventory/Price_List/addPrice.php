abcd8adf2c3d5e65674d6cfbaf923abee89e7729
Alexander Inciong <xnamecf04@gmail.com>
2024-04-16 10:40:16 +0800

    Update addPrice.php
    Azul to

--- Inventory/Price_List/addPrice.php
+++ Inventory/Price_List/addPrice.php
@@ -1,15 +1,17 @@
 <?php
+    require_once '../../admin/session.php';
     require_once '../../database/database.php';
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $query = 'INSERT INTO price_list
-                (product_id, dealer, wholesale, srp)
-                VALUES (?,?,?,?)';
+                (product_id, dealer, wholesale, srp, publish_by)
+                VALUES (?,?,?,?,?)';
         $stmt = $conn->prepare($query);
-        $stmt->bind_param('iiii', $_POST['product_id'], $_POST['dealer'], $_POST['wholesale'], $_POST['srp']);
+        $stmt->bind_param('iiiii', $_POST['product_id'], $_POST['dealer'], $_POST['wholesale'], $_POST['srp'], $user_id);
         $stmt->execute();
         $stmt->close();
 
         header('Location: ../Price_List/?success=true');
+        exit;
     }
 ?>
