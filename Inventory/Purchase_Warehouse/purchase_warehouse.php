256f3936d7509ace4589fde74b4ebb7db8ac592b
Fyke Lleva <floterina@gmail.com>
2024-04-17 17:14:25 +0800

    update

--- Inventory/Purchase_Warehouse/purchase_warehouse.php
+++ Inventory/Purchase_Warehouse/purchase_warehouse.php
@@ -82,7 +82,7 @@ while ($stmt->fetch()) {
                 \''.$unit_name.'\', 
                 \''.$models.'\', 
                 \''.$srp.'\', 
-                \''.0.'\'
+                \''.$total_stocks.'\'
                 )"><span class="fas fa-cart-plus "></span></button>
     </td>
 
