7b64e6ed93f39615543c2d2c761bc390fb4c05af
jaygun211 <162282449+Jaygun211@users.noreply.github.com>
2024-04-18 17:40:05 +0800

    tito jgun upodate

---
+++ DMP/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Company</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header class="bg-dark text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-12 d-flex align-items-center">
+                    <img src="path/to/your/logo.png" alt="Company Logo" width="100" height="100" class="mr-3">
+                    <!-- Center the company name -->
+                    <h1 class="h4 my-0 mx-auto">Danielle Motor Parts</h1>
+                </div>
+                <div class="col-md-6 col-sm-12 d-flex justify-content-end align-items-center">
+                    <nav>
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item"><a href="products.php" class="text-white">Products</a></li>
+                            <li class="list-inline-item"><a href="contact.php" class="text-white">Contact</a></li>
+                            <li class="list-inline-item"><a href="login.php" class="text-white">Login/Register</a></li>
+                            <li class="list-inline-item"><a href="cart.php" class="text-white">Cart</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Bootstrap JS (optional) -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
