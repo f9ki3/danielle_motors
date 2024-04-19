92ed22fc72a30ee19d8151951f7deba48d79adca
jaygun211 <162282449+Jaygun211@users.noreply.github.com>
2024-04-19 09:54:55 +0800

    file structure

---
+++ rename/Default Starter/swiper_002.php
@@ -0,0 +1,47 @@
+<div class="d-flex flex-between-center mb-3">
+    <h3>Best Offers</h3>
+    <a class="fw-bold d-none d-md-block" href="#!">Explore more<span class="fas fa-chevron-right fs--1 ms-1"></span></a>
+</div>
+
+<div class="swiper-theme-container products-slider">
+    <div class="swiper swiper-container theme-slider" data-swiper='{"slidesPerView":1,"spaceBetween":16,"breakpoints":{"450":{"slidesPerView":2,"spaceBetween":16},"576":{"slidesPerView":3,"spaceBetween":20},"768":{"slidesPerView":4,"spaceBetween":20},"992":{"slidesPerView":5,"spaceBetween":20},"1200":{"slidesPerView":6,"spaceBetween":16}}}'>
+        <div class="swiper-wrapper">
+            <!-- start loop here -->
+            <div class="swiper-slide">
+                <div class="position-relative text-decoration-none product-card h-100">
+                    <div class="d-flex flex-column justify-content-between h-100">
+                        <div>
+                            <div class="border border-1 rounded-3 position-relative mb-3">
+                                <button class="btn rounded-circle p-0 d-flex flex-center btn-wish z-index-2 d-toggle-container btn-outline-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="Add to wishlist">
+                                    <span class="fas fa-heart d-block-hover"></span>
+                                    <span class="far fa-heart d-none-hover"></span>
+                                </button>
+                                <img class="img-fluid" src="../../assets/img/products/25.png" alt="" />
+                            </div>
+                            <a class="stretched-link" href="product-details.html">
+                                <h6 class="mb-2 lh-sm line-clamp-3 product-name">RESPAWN 200 Racing Style Gaming Chair, in Gray RSP 200 GRY</h6>
+                            </a>
+                            <p class="fs--1">
+                                <span class="fa fa-star text-warning"></span>
+                                <span class="fa fa-star text-warning"></span>
+                                <span class="fa fa-star text-warning"></span>
+                                <span class="fa fa-star text-warning"></span>
+                                <span class="fa fa-star text-warning"></span>
+                            </p>
+                        </div>
+                        <div>
+                            <h6 class="text-success lh-1 mb-0">35% off</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end loop here -->
+        </div>
+    </div>
+    <div class="swiper-nav">
+        <div class="swiper-button-next"><span class="fas fa-chevron-right nav-icon"></span></div>
+        <div class="swiper-button-prev"><span class="fas fa-chevron-left nav-icon"></span></div>
+    </div>
+</div>
+
+<a class="fw-bold d-md-none" href="#!">Explore more<span class="fas fa-chevron-right fs--1 ms-1"></span></a>
