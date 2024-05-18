b2eeb972167517206f56c78b3eb79fd234a676f5
Fyke Lleva <floterina@gmail.com>
2024-05-18 16:04:59 +0800

    expense limit

--- Inventory/Purchased-Order-Supplier/update_expense.php
+++ Inventory/Purchased-Order-Supplier/update_expense.php
@@ -1,6 +1,21 @@
-<?php 
+<?php
 include "../../admin/session.php";
 include "../../database/database.php";
 
-$update_expense = "UPDATE expense_limit SET warning "
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Assuming $branch_code is properly set before this point
+    $warning = $_POST['warning_level'];
+    $limit = $_POST['danger_level'];
+    
+    // Prepare and execute the SQL query
+    $update_expense = "UPDATE expense_limit SET warning_expense = '$warning', limit_expense = '$limit' WHERE branch_code = '$branch_code'";
+    
+    if ($conn->query($update_expense) === TRUE) {
+        $conn->close();
+        header("Location: ../Purchased-Order-Supplier/?update=success");
+        exit;
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
 ?>
