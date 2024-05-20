d46ea3561a213c4be2d707c7f84050006b76b4c8
Fyke Lleva <floterina@gmail.com>
2024-05-20 22:41:35 +0800

    Revert "updated"
    This reverts commit 700e7776e60c1c8754ee15af04b72f71571222c1.

--- POS/Dashboard/content.php
+++ POS/Dashboard/content.php
@@ -92,18 +92,6 @@
         
     </div>
 <hr>
-    <div class="row">
-        <div class="col-12 col-md-6 p-2">
-            <div id="pos_bar">
-
-            </div>
-        </div>
-        <div class="col-12 col-md-6 p-2">
-            <div id="line">
-
-            </div>
-        </div>
-    </div>
     <div  class=" p-3 mb-3 w-100">
         <div class="row">
             <div class="col-12 col-md-6 p-2">
