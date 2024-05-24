e37e32024fa9c4c020937e77eb46aed4a9359b31
Christian Azul <53144221+ChristianAzulL@users.noreply.github.com>
2024-05-24 15:02:30 +0800

    somethings

--- PHP - process_files/save_to_stocks.php
+++ PHP - process_files/save_to_stocks.php
@@ -88,6 +88,6 @@ if (isset($branch_code)) {
 }
 
 // Redirect with response
-header("Location: ../Inventory/barcode_scanner%20copy/?successful=$response");
+header("Location: ../Inventory/all_stocks/?successful=$response");
 exit;
 ?>
