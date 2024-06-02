135014c8197430a6aa7f3f6502c005df263b04c9
Zelziee <119650160+DySyre@users.noreply.github.com>
2024-06-02 14:18:13 +0800

    login at logout

--- PHP - process_files/signout.php
+++ PHP - process_files/signout.php
@@ -1,22 +1,22 @@
 <?php
 include '../admin/session.php';
 include '../config/config.php';
-// Start the session
-session_start();
+// Set timezone to Manila (GMT+8)
+date_default_timezone_set('Asia/Manila');
+$currentTimestamp = date('Y-m-d H:i:s');
 
 // Get user ID and branch code from the session
 $id = $_SESSION['id'];
 $user_brn_code = $_SESSION['user_brn_code'];
 
 // Prepare the SQL statement to insert the audit log
-$stmt_log = $conn->prepare("INSERT INTO `audit` (`id`, `audit_user_id`, `audit_date`, `audit_action`, `audit_description`, `user_brn_code`) VALUES (NULL, ?, NOW(), 'logout', 'logout inventory', ?)");
+$stmt_log = $conn->prepare("INSERT INTO `audit` (`id`, `audit_user_id`, `audit_date`, `audit_action`, `audit_description`, `user_brn_code`) VALUES (NULL, ?, ?, 'logout', 'logout inventory', ?)");
+$stmt_log->bind_param("iss", $id, $currentTimestamp, $user_brn_code);
+$stmt_log->execute();
 if ($stmt_log === false) {
     die('Error: ' . htmlspecialchars($conn->error));
 }
-// Bind parameters
-$stmt_log->bind_param("is", $id, $user_brn_code); 
-$stmt_log->execute();
-$_SESSION = array();
+
 // Destroy the session
 session_destroy();
 
